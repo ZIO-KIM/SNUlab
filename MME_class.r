64df26cc72a71b88e8a7d4c368833bb6862297dc
Zio Kim <zio0323gm@gmail.com>
2021-11-17 13:28:00 +0900

    1117

--- MME_class.r
+++ MME_class.r
@@ -1,4 +1,7 @@
 
+
+######## 서원진 선생님이 MPlus로 주신 데이터 전처리하는 코드 #########
+
 #### library ####
 library(dplyr)
 library(forecast)
@@ -19,78 +22,77 @@ mme_f2r <- read.table('data\\MME_F2R.dat', encoding = 'euc-kr')
 mme_f3r <- read.table('data\\MME_F3R.dat', encoding = 'euc-kr')
 mme_m2r <- read.table('data\\MME_M2R.dat', encoding = 'euc-kr')
 mme_m3r <- read.table('data\\MME_M3R.dat', encoding = 'euc-kr')
-mme_f <- read.table('data\\MME_F.dat', encoding = 'euc-kr')
-mme_m <- read.table('data\\MME_M.dat', encoding = 'euc-kr')
+mme_f <- read.table('data\\MME_FGO.dat', encoding = 'euc-kr')
+mme_m <- read.table('data\\MME_MGO.dat', encoding = 'euc-kr')
 
 sum(is.na(mme_f))
 
 # ckd = read.csv('CKD_final_Baseline1st+OccuredTwiceOrMoreInARow.csv', encoding = 'euc-kr')
 
-# # change V36, V37 to class
-# names(mme_f2r)[names(mme_f2r) == 'V36'] <- 'CLASS'
-# names(mme_f3r)[names(mme_f3r) == 'V37'] <- 'CLASS'
-# names(mme_m2r)[names(mme_m2r) == 'V36'] <- 'CLASS'
-# names(mme_m3r)[names(mme_m3r) == 'V37'] <- 'CLASS'
+# change V33, V34 to class
+names(mme_f2r)[names(mme_f2r) == 'V33'] <- 'CLASS'
+names(mme_f3r)[names(mme_f3r) == 'V34'] <- 'CLASS'
+names(mme_m2r)[names(mme_m2r) == 'V33'] <- 'CLASS'
+names(mme_m3r)[names(mme_m3r) == 'V34'] <- 'CLASS'
 
-# # cbind
-# mme_f2r <- cbind(mme_f, mme_f2r[c('CLASS')])
-# mme_f3r <- cbind(mme_f, mme_f3r[c('CLASS')])
-# mme_m2r <- cbind(mme_m, mme_m2r[c('CLASS')])
-# mme_m3r <- cbind(mme_m, mme_m3r[c('CLASS')])
+# cbind
+mme_f2r <- cbind(mme_f, mme_f2r[c('CLASS')])
+mme_f3r <- cbind(mme_f, mme_f3r[c('CLASS')])
+mme_m2r <- cbind(mme_m, mme_m2r[c('CLASS')])
+mme_m3r <- cbind(mme_m, mme_m3r[c('CLASS')])
 
-# # add column names
-# add_columns <- function(df) {
-#     names(df)[names(df) == 'V1'] <- '기수'
-#     names(df)[names(df) == 'V2'] <- 'EDATE'
-#     names(df)[names(df) == 'V3'] <- 'NIHID'
-#     names(df)[names(df) == 'V4'] <- 'AGE'
-#     names(df)[names(df) == 'V5'] <- 'SEX'
-#     names(df)[names(df) == 'V6'] <- 'HEIGHT'
-#     names(df)[names(df) == 'V7'] <- 'WEIGHT'
-#     names(df)[names(df) == 'V8'] <- 'WAIST'
-#     names(df)[names(df) == 'V9'] <- 'GLU0_ORI'
-#     names(df)[names(df) == 'V10'] <- 'R_GTP_TR'
-#     names(df)[names(df) == 'V11'] <- 'AST_ORI'
-#     names(df)[names(df) == 'V12'] <- 'ALT_ORI'
-#     names(df)[names(df) == 'V13'] <- 'TCHL_ORI'
-#     names(df)[names(df) == 'V14'] <- 'HDL_ORI'
-#     names(df)[names(df) == 'V15'] <- 'TRIGLY_ORI'
-#     names(df)[names(df) == 'V16'] <- 'HB_ORI'
-#     names(df)[names(df) == 'V17'] <- 'SMOKE'
-#     names(df)[names(df) == 'V18'] <- 'DRUGINS'
-#     names(df)[names(df) == 'V19'] <- 'DRUGHT'
-#     names(df)[names(df) == 'V20'] <- 'TREATD5'
-#     names(df)[names(df) == 'V21'] <- 'DRUGICD'
-#     names(df)[names(df) == 'V22'] <- 'DRUGLP'
-#     names(df)[names(df) == 'V23'] <- 'FMHTN'
-#     names(df)[names(df) == 'V24'] <- 'FMHEA'
-#     names(df)[names(df) == 'V25'] <- 'FMDM'
-#     names(df)[names(df) == 'V26'] <- 'PRT16_U'
-#     names(df)[names(df) == 'V27'] <- 'TREATD14'
-#     names(df)[names(df) == 'V28'] <- 'KID'
-#     names(df)[names(df) == 'V29'] <- 'KIDAG'
-#     names(df)[names(df) == 'V30'] <- 'KIDCU'
-#     names(df)[names(df) == 'V31'] <- 'TOTALC'
-#     names(df)[names(df) == 'V32'] <- 'PHYACTL'
-#     names(df)[names(df) == 'V33'] <- 'PHYACTM'
-#     names(df)[names(df) == 'V34'] <- 'PHYACTH'
-#     names(df)[names(df) == 'V35'] <- 'BODYFAT'
-#     names(df)[names(df) == 'V36'] <- 'MET_CAL'
-#     names(df)[names(df) == 'V37'] <- 'PA_NEW'
-#     names(df)[names(df) == 'V38'] <- 'SBP'
-#     names(df)[names(df) == 'V39'] <- 'DBP'
-#     names(df)[names(df) == 'V40'] <- 'eGFR'
-#     names(df)[names(df) == 'V41'] <- 'BMI'
-#     names(df)[names(df) == 'V42'] <- 'DRK_NEW'
+# add column names
+add_columns <- function(df) {
+    names(df)[names(df) == 'V1'] <- '기수'
+    names(df)[names(df) == 'V2'] <- 'EDATE'
+    names(df)[names(df) == 'V3'] <- 'NIHID'
+    names(df)[names(df) == 'V4'] <- 'AGE'
+    names(df)[names(df) == 'V5'] <- 'SEX'
+    names(df)[names(df) == 'V6'] <- 'HEIGHT'
+    names(df)[names(df) == 'V7'] <- 'WEIGHT'
+    names(df)[names(df) == 'V8'] <- 'WAIST'
+    names(df)[names(df) == 'V9'] <- 'GLU0_ORI'
+    names(df)[names(df) == 'V10'] <- 'R_GTP_TR'
+    names(df)[names(df) == 'V11'] <- 'AST_ORI'
+    names(df)[names(df) == 'V12'] <- 'ALT_ORI'
+    names(df)[names(df) == 'V13'] <- 'TCHL_ORI'
+    names(df)[names(df) == 'V14'] <- 'HDL_ORI'
+    names(df)[names(df) == 'V15'] <- 'TRIGLY_ORI'
+    names(df)[names(df) == 'V16'] <- 'HB_ORI'
+    names(df)[names(df) == 'V17'] <- 'SMOKE'
+    names(df)[names(df) == 'V18'] <- 'DRUGINS'
+    names(df)[names(df) == 'V19'] <- 'DRUGHT'
+   
+    names(df)[names(df) == 'V20'] <- 'DRUGICD'
+    names(df)[names(df) == 'V21'] <- 'DRUGLP'
+    names(df)[names(df) == 'V22'] <- 'FMHTN'
+    names(df)[names(df) == 'V23'] <- 'FMHEA'
+    names(df)[names(df) == 'V24'] <- 'FMDM'
+    names(df)[names(df) == 'V25'] <- 'PRT16_U'
+ 
+    names(df)[names(df) == 'V26'] <- 'KID'
 
-#     return (df)
-# }
-# mme_f2r <- add_columns(mme_f2r)
-# mme_f3r <- add_columns(mme_f3r)
-# mme_m2r <- add_columns(mme_m2r)
-# mme_m3r <- add_columns(mme_m3r)
+    names(df)[names(df) == 'V27'] <- 'TOTALC'
+    names(df)[names(df) == 'V28'] <- 'PHYACTL'
+    names(df)[names(df) == 'V29'] <- 'PHYACTM'
+    names(df)[names(df) == 'V30'] <- 'PHYACTH'
 
-# # # dummy class
+    names(df)[names(df) == 'V31'] <- 'MET_CAL'
+    names(df)[names(df) == 'V32'] <- 'PA_NEW'
+    names(df)[names(df) == 'V33'] <- 'SBP'
+    names(df)[names(df) == 'V34'] <- 'DBP'
+    names(df)[names(df) == 'V35'] <- 'eGFR'
+    names(df)[names(df) == 'V36'] <- 'BMI'
+    names(df)[names(df) == 'V37'] <- 'DRK_NEW'
+
+    return (df)
+}
+mme_f2r <- add_columns(mme_f2r)
+mme_f3r <- add_columns(mme_f3r)
+mme_m2r <- add_columns(mme_m2r)
+mme_m3r <- add_columns(mme_m3r)
+
+# # # dummy class - 하지말것
 # # # 남자 2그룹 
 # # mme_m2r <- mme_m2r %>%
 # #     mutate(CLASS_1 = if_else(CLASS == 1, 1, 0)) %>%
@@ -118,24 +120,29 @@ sum(is.na(mme_f))
 # # mme_f3r <- subset(mme_f3r, select = -c(CLASS))
 
 
-# # reassign female class values 1,2 to 3,4
-# mme_f2r$CLASS[mme_f2r$CLASS == 1] <- 3
-# mme_f2r$CLASS[mme_f2r$CLASS == 2] <- 4
+# reassign female class values 1,2 to 3,4
+mme_f2r$CLASS[mme_f2r$CLASS == 1] <- 3
+mme_f2r$CLASS[mme_f2r$CLASS == 2] <- 4
 
-# mme_f3r$CLASS[mme_f3r$CLASS == 1] <- 4
-# mme_f3r$CLASS[mme_f3r$CLASS == 2] <- 5
-# mme_f3r$CLASS[mme_f3r$CLASS == 3] <- 6
+mme_f3r$CLASS[mme_f3r$CLASS == 1] <- 4
+mme_f3r$CLASS[mme_f3r$CLASS == 2] <- 5
+mme_f3r$CLASS[mme_f3r$CLASS == 3] <- 6
 
-# # create group2, group3 finaldf
-# mme_group2 <- rbind(mme_m2r, mme_f2r)
-# mme_group3 <- rbind(mme_m3r, mme_f3r)
+# create group2, group3 finaldf
+mme_group2 <- rbind(mme_m2r, mme_f2r)
+mme_group3 <- rbind(mme_m3r, mme_f3r)
 
 # # merge CKD
 # final_group2 = merge(mme_group2, ckd, by = 'NIHID')
 # final_group3 = merge(mme_group3, ckd, by = 'NIHID')
 
-# write.csv(final_group2, file = 'MME_group2.csv', row.names = FALSE)
-# write.csv(final_group3, file = 'MME_group3.csv', row.names = FALSE)
+write.csv(mme_group2, file = 'data\\MME_group2.csv', row.names = FALSE)
+write.csv(mme_group3, file = 'data\\MME_group3.csv', row.names = FALSE)
+
+write.csv(mme_m2r, file = 'data\\MME_Mgroup2.csv', row.names = FALSE)
+write.csv(mme_m3r, file = 'data\\MME_Mgroup3.csv', row.names = FALSE)
+write.csv(mme_f2r, file = 'data\\MME_Fgroup2.csv', row.names = FALSE)
+write.csv(mme_f3r, file = 'data\\MME_Fgroup3.csv', row.names = FALSE)
 
 final_group2 = read.csv('data\\MME_group2_preprocessed.csv', encoding = 'euc-kr')
 final_group3 = read.csv('data\\MME_group3_preprocessed.csv', encoding = 'euc-kr')
