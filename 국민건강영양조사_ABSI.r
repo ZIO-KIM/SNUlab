b02488ee0cdac70a38e8eef0de379027097d1e71
Zio Kim <zio0323gm@gmail.com>
2022-03-14 21:25:06 +0900

    0314

--- 국민건강영양조사_ABSI.r
+++ 국민건강영양조사_ABSI.r
@@ -4,6 +4,7 @@
 # install.packages("httpgd")
 # install.packages("knitr")
 # install.packages("rmarkdown")
+install.packages("ggplot2")
 
 #### library ####
 library(dplyr)
@@ -27,6 +28,8 @@ library(lubridate)
 library(ape)
 library(knitr)
 library(rmarkdown)
+library(survival)
+library(ggplot2)
 #################
 
 
@@ -142,3 +145,209 @@ summary(model5)
 
 model5 <- lm(BMI ~ WSTC, data=ukb_1st_final)
 summary(model5)
+
+#### --------------------------------------------------------------------------------------------- ####
+
+
+# ABSI 모델링
+
+htn_df <- read.csv('0. data\\국건영_ABSI\\absi_htn.csv', encoding = 'euc-kr')
+dm_df <- read.csv('0. data\\국건영_ABSI\\absi_dm.csv', encoding = 'euc-kr')
+mi_df <- read.csv('0. data\\국건영_ABSI\\absi_mi.csv', encoding = 'euc-kr')
+lip_df <- read.csv('0. data\\국건영_ABSI\\absi_lip.csv', encoding = 'euc-kr')
+ceva_df <- read.csv('0. data\\국건영_ABSI\\absi_ceva.csv', encoding = 'euc-kr')
+gout_df <- read.csv('0. data\\국건영_ABSI\\absi_gout.csv', encoding = 'euc-kr')
+
+#### copy df to use
+
+# HTN
+df <- copy(htn_df)
+target <- df$HTN_final
+target_name <- "HTN_final"
+
+# DM
+df <- copy(dm_df)
+target <- df$DM_final
+target_name <- "DM_final"
+
+# MI
+df <- copy(mi_df)
+target <- df$MI_final
+target_name <- "MI_final"
+
+# LIP
+df <- copy(lip_df)
+target <- df$LIP_final
+target_name <- "LIP_final"
+
+# CEVA
+df <- copy(ceva_df)
+target <- df$CEVA_final
+target_name <- "CEVA_final"
+
+# GOUT
+df <- copy(gout_df)
+target <- df$GOUT_final
+target_name <- "GOUT_final"
+
+###################
+
+
+#### 추가 전처리 및 factor
+
+# 중간처리 변수들 빼기
+df <- subset(df, select = -c(EDATE, maxdate, mindate, 평균, 표준편차))
+
+df$SMOKE <- factor(df$SMOKE)
+df$DRINK <- factor(df$DRINK)
+
+###################
+
+
+#### Stepwise variable selection ####
+
+# variables 리스트 생성
+variables <- colnames(df)
+
+# 선택되면 안되는 변수들 빼기
+variables <- variables[!variables %in% c(target_name, "기수", "NIHID", "VISITALL", "TIME", "BMI", 
+"ABSI", "ABSI_Z", "HTN_age", "DM_age", 'MI_age', "LIP_age", "CEVA_age", "GOUT_age")]
+
+df2 <- df[variables]
+
+full <- glm(target ~ ., family = binomial(link = "logit"), data = df2)
+step_lm <- step(full, direction = "both")
+
+summary(step_lm)
+
+step_variables <- c(all.vars(formula(step_lm)[[3]])) # stepwise로 선택된 변수들만 가져오기
+
+step_variables
+
+#########################
+
+
+#### GLM ####
+
+# create glm formula - ABSI only
+variables <- c(step_variables, "ABSI_Z")
+variables <- variables[!variables %in% c('WAIST', 'HEIGHT', 'WEIGHT')]
+
+glm_f <- as.formula(
+  paste(target_name,
+    paste(variables, collapse = " + "),
+    sep = " ~ "
+  )
+)
+
+glm_f
+
+# fit
+fit <- glm(glm_f, family = binomial(link = "logit"), data = df)
+summary(fit)
+
+
+
+# create glm formula - BMI only
+variables <- c(step_variables, "BMI")
+variables <- variables[!variables %in% c('WAIST', 'HEIGHT', 'WEIGHT')]
+
+glm_f <- as.formula(
+  paste(target_name,
+    paste(variables, collapse = " + "),
+    sep = " ~ "
+  )
+)
+
+glm_f
+
+# fit
+fit <- glm(glm_f, family = binomial(link = "logit"), data = df)
+summary(fit)
+
+
+
+# create glm formula - ABSI_Z & BMI
+variables <- c(step_variables, "ABSI_Z", "BMI")
+variables <- variables[!variables %in% c('WAIST', 'HEIGHT', 'WEIGHT')]
+
+glm_f <- as.formula(
+  paste(target_name,
+    paste(variables, collapse = " + "),
+    sep = " ~ "
+  )
+)
+
+glm_f
+
+# fit
+fit <- glm(glm_f, family = binomial(link = "logit"), data = df)
+summary(fit)
+
+
+
+#### COX ####
+    
+    # ABSI_Z only
+    variables <- c(step_variables, "ABSI_Z")
+    variables <- variables[!variables %in% c('WAIST', 'HEIGHT', 'WEIGHT')]
+
+    x = paste(variables, collapse = " + ")
+    y = "Surv(TIME, target)"
+    cox_f = as.formula(paste(y, "~", x))
+
+    cox_f
+
+    # fit
+    fit <- coxph(cox_f, data = df) # nolint
+    summary(fit)
+
+    # visualization
+    ggsurvplot(surv_fit(fit, data = df), data = df, risk.table = TRUE)
+
+    # LML plot
+    ggsurvplot(surv_fit(fit, data = df), data = df, fun = "cloglog") # nolint
+
+
+
+
+    # BMI only
+    variables <- c(step_variables, "BMI")
+    variables <- variables[!variables %in% c('WAIST', 'HEIGHT', 'WEIGHT')]
+
+    x = paste(variables, collapse = " + ")
+    y = "Surv(TIME, target)"
+    cox_f = as.formula(paste(y, "~", x))
+
+    # fit
+    fit <- coxph(cox_f, data = df) # nolint
+    summary(fit)
+
+    # visualization
+    ggsurvplot(surv_fit(fit, data = df), data = df, risk.table = TRUE)
+
+    # LML plot
+    ggsurvplot(surv_fit(fit, data = df), data = df, fun = "cloglog") # nolint
+
+
+
+
+    # ABSI_Z + BMI
+    variables <- c(step_variables, "ABSI_Z", "BMI")
+    variables <- variables[!variables %in% c('WAIST', 'HEIGHT', 'WEIGHT')]
+
+    x = paste(variables, collapse = " + ")
+    y = "Surv(TIME, target)"
+    cox_f = as.formula(paste(y, "~", x))
+
+    # fit
+    fit <- coxph(cox_f, data = df) # nolint
+    summary(fit)
+
+    # visualization
+    ggsurvplot(surv_fit(fit, data = df), data = df, risk.table = TRUE)
+
+    # LML plot
+    ggsurvplot(surv_fit(fit, data = df), data = df, fun = "cloglog") # nolint
+    
+#####################
